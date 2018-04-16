c17774853c6c1a06be81b777e463368043db92fd
Victorhusky <31705315+Victorhusky@users.noreply.github.com>
2018-04-16 18:35:46 -0400

    s

--- TripPlanning_V1/app/controllers/HomeController.scala
+++ TripPlanning_V1/app/controllers/HomeController.scala
@@ -2,6 +2,13 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import scala.util.parsing.json.JSON._
+import scala.util.parsing.json.JSONObject
+import play.api.libs.json
+import play.api.libs.json.__
+import
+
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -11,14 +18,28 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action {
+    show()
     //Ok(views.html.index("Your new application is ready."))
     Ok(views.html.home.render())
+
   }
 
+  def show(): Unit = {
+    val str = "{\"et\":\"kanqiu_client_join\",\"vtm\":1435898329434,\"body\":{\"client\":\"866963024862254\",\"client_type\":\"android\",\"room\":\"NBA_HOME\",\"gid\":\"\",\"type\":\"\",\"roomid\":\"\"},\"time\":1435898329}"
+    val result = (str)
+
+    result match {
+      // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
+      case Some(map: Map[String, Any]) => println(map)
+      case None => println("Parsing failed")
+      case other => println("Unknown data structure: " + other)
+
+    }
+  }
 }
--- TripPlanning_V1/build.sbt
+++ TripPlanning_V1/build.sbt
@@ -1,7 +1,7 @@
 name := "TripPlanning_V1"
  
-version := "1.0" 
-      
+version := "1.0"
+
 lazy val `tripplanning_v1` = (project in file(".")).enablePlugins(PlayScala)
 
 resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
@@ -13,5 +13,3 @@ scalaVersion := "2.12.2"
 libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
-
-      
